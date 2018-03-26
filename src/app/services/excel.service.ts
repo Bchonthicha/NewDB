1c55ba30cb0141ceef9a58c287805d9375111115
Bchonthicha <chonthicha_b@cmu.ac.th>
2018-03-27 04:28:41 +0700

    v1

---
+++ src/app/services/excel.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+import * as XLSX from 'xlsx';
+
+const EXCEL_EXTENSION = '.xlsx';
+
+@Injectable()
+export class ExcelService {
+
+  constructor() { }
+
+  public exportAsExcelFile(json: any[], excelFileName: string): void {
+
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
+
+    const workbook: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Student');   // worksheet name
+
+    XLSX.writeFile(workbook, excelFileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);  //exel name
+
+  }
+
+}
